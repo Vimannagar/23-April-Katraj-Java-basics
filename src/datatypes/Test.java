57d2af49ca91543209e151db7189c4a6ec565c25
Gourav Jawale <velocity.ctc@gmail.com>
2022-05-25 10:13:31 +0530

    Static method

---
+++ src/datatypes/Test.java
@@ -0,0 +1,46 @@
+package datatypes;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		int j = 20;
+		
+		int k = i+j;
+		
+		System.out.println(k);
+		
+		int l = i-j;
+		
+		System.out.println(l);
+		
+		
+		int m	= i*j;
+	
+		System.out.println(m);
+	
+	
+		double n = i/j;
+		
+		double g = 10;
+		double h = 20;
+		double o = g/h;
+		
+		System.out.println(o);
+	
+		System.out.println(n);
+		
+		float p = -455.56f;
+		
+		System.out.println(p);
+		
+		
+//		byte --> short --> int --> long -->float--> double
+//				 char
+		
+		
+	}
+
+}
---
+++ src/methods/StaticMethods.java
@@ -0,0 +1,39 @@
+package methods;
+
+public class StaticMethods {
+	
+//	syntax:		 access_modifier static return_type name_of_method()
+//				{
+//		
+//				}
+	
+	
+	
+	
+	public static void printHello()
+	{
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+	
+public static void main(String[] args) {
+	printHello();
+	addition();
+}
+
+public static void addition()
+{
+	int i  = 50;
+	int j = 20;
+	
+	int k = i+j;
+	
+	System.out.println(k);
+}
+
+
+
+}
